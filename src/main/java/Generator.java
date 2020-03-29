30bd4ba8f6d6b51fd1de4732f99050ee44205fcb
lenovo <suleimanovayev@gmail.com>
2020-03-29 10:19:15 +0300

    small fixed2

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -11,33 +11,35 @@ import service.impl.Triangle;
 import static java.time.chrono.JapaneseEra.values;
 
 public class Generator {
+    private static final int VARIABLE_VALUE = 100;
+    private static int subclasses = 4;
 
     public static List<Figure> randomFigure(int quantity) {
         List<Figure> array = new ArrayList<>();
         Figure figure = null;
         Random random = new Random();
         for (int i = 0; i < quantity; i++) {
-            int randomNumber = random.nextInt(4);
+            int randomNumber = random.nextInt(subclasses);
 
             switch (randomNumber) {
                 case 0:
-                    float radius = random.nextFloat() * 100;
+                    float radius = random.nextFloat() * VARIABLE_VALUE;
                     figure = new Circle(radius);
                     break;
                 case 1:
-                    float side = random.nextFloat() * 100;
+                    float side = random.nextFloat() * VARIABLE_VALUE;
                     figure = new Square(side);
                     break;
                 case 2:
-                    float a = random.nextFloat() * 100;
-                    float b = random.nextFloat() * 100;
-                    float c = random.nextFloat() * 100;
+                    float a = random.nextFloat() * VARIABLE_VALUE;
+                    float b = random.nextFloat() * VARIABLE_VALUE;
+                    float c = random.nextFloat() * VARIABLE_VALUE;
                     figure = new Trapezium(a, b, c);
                     break;
                 case 3:
-                    float d = random.nextFloat() * 100;
-                    float f = random.nextFloat() * 100;
-                    float g = random.nextFloat() * 100;
+                    float d = random.nextFloat() * VARIABLE_VALUE;
+                    float f = random.nextFloat() * VARIABLE_VALUE;
+                    float g = random.nextFloat() * VARIABLE_VALUE;
                     figure = new Triangle(d, f, g);
                     break;
             }
